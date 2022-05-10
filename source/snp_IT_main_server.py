d66f1d19db0605f1ded8814699067d9d89cc239e
Andrey Sopov <saa020403@gmail.com>
2022-05-10 20:19:29 +0300

    refactor

---
+++ source/snp_IT_main_server.py
@@ -0,0 +1,19 @@
+import tornado.ioloop
+import tornado.web
+
+
+class MainHandler(tornado.web.RequestHandler):
+    def get(self) -> None:
+        self.render("../templates/main.html")
+
+
+def make_app() -> tornado.web.Application:
+    return tornado.web.Application([
+        (r"/", MainHandler),
+    ])
+
+
+if __name__ == "__main__":
+    app = make_app()
+    app.listen(8888)
+    tornado.ioloop.IOLoop.current().start()
