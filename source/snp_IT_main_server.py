bd12240467ed505993b878f3cf8afde580af796c
Andrey Sopov <saa020403@gmail.com>
2022-05-16 12:52:21 +0300

    plug in styles and scripts into tornado

--- source/snp_IT_main_server.py
+++ source/snp_IT_main_server.py
@@ -1,17 +1,21 @@
 import tornado.ioloop
 import tornado.web
+import os
 import cv2
 
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self) -> None:
-        self.render("../templates/main.html")
+        self.render("main.html", )
 
 
 def make_app() -> tornado.web.Application:
     return tornado.web.Application([
-        (r"/", MainHandler),
-    ])
+        (r'/', MainHandler)],
+        static_path=os.path.join(os.path.dirname(f'{__file__}/../../'),
+                                 'static'),
+        template_path=os.path.join(os.path.dirname(f'{__file__}/../../'),
+                                   'templates'))
 
 
 if __name__ == "__main__":
