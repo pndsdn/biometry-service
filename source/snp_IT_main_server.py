6989db60cfbc9f5ca4a70a2d0d65183e6d17e0ae
Andrey Sopov <saa020403@gmail.com>
2022-05-16 18:12:40 +0300

    request handler POST in progress

--- source/snp_IT_main_server.py
+++ source/snp_IT_main_server.py
@@ -1,17 +1,26 @@
 import tornado.ioloop
 import tornado.web
-import os
+import os.path
 import cv2
 
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self) -> None:
-        self.render("main.html", )
+        self.render("main.html")
+
+
+class UploadHandler(tornado.web.RequestHandler):
+    def post(self):
+        file = self.request.files['file']
+        print(file[0]['filename'])
+        self.redirect('http://localhost:8888/')
+
 
 
 def make_app() -> tornado.web.Application:
     return tornado.web.Application([
-        (r'/', MainHandler)],
+        (r'/', MainHandler),
+        (r'/upload', UploadHandler)],
         static_path=os.path.join(os.path.dirname(f'{__file__}/../../'),
                                  'static'),
         template_path=os.path.join(os.path.dirname(f'{__file__}/../../'),
