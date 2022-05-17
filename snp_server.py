b35df201d0962808dde4b6ae2f797ba2414ba6af
Andrey Sopov <saa020403@gmail.com>
2022-05-18 00:37:41 +0300

    minor changes

--- snp_server.py
+++ snp_server.py
@@ -17,7 +17,7 @@ class UploadHandler(tornado.web.RequestHandler):
                                f'/uploads/{file["filename"]}', 'wb')
             output_file.write(file['body'])
         except Exception:
-            print('WrongPOSTParams: file have not been attached)')
+            print('Error: file have not been attached')
 
         self.redirect('http://localhost:8888/')
 
